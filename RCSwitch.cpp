5f2a74a371d01d57a88620b677540f5d9bc55844
codeartisan.de <codeartisan.de@gmail.com>
2019-12-15 21:56:45 +0100

    Fixed 2 missed merge conflicts an change my Mandoly Protocol to ID Nr 8.

--- RCSwitch.cpp
+++ RCSwitch.cpp
@@ -79,7 +79,8 @@ static const RCSwitch::Protocol PROGMEM proto[] = {
   { 380, {  1,  6 }, {  1,  3 }, {  3,  1 }, false },    // protocol 4
   { 500, {  6, 14 }, {  1,  2 }, {  2,  1 }, false },    // protocol 5
   { 450, { 23,  1 }, {  1,  2 }, {  2,  1 }, true },     // protocol 6 (HT6P20B)
-  { 636, {  1, 10 }, {  1,  2 }, {  2,  1 }, true },     // protocol 7 (Mandolyn/Lidl TR-502MSV/RC-402/RC-402DX): https://github.com/sui77/rc-switch/pull/55
+  { 150, {  2, 62 }, {  1,  6 }, {  6,  1 }, false },    // protocol 7 (HS2303-PT, i. e. used in AUKEY Remote)
+  { 636, {  1, 10 }, {  1,  2 }, {  2,  1 }, true }      // protocol 8 (Mandolyn/Lidl TR-502MSV/RC-402/RC-402DX): https://github.com/sui77/rc-switch/pull/55
 };
 
 enum {
@@ -504,11 +505,6 @@ void RCSwitch::send(unsigned long code, unsigned int length) {
         this->transmit(protocol.zero);
     }
     this->transmit(protocol.syncFactor);
-  }
-
-  if (protocol.invertedSignal) {
-	  // for "low starting" (and therefore "high ending") protocols set the transmitter back to low (no signal).
-	  digitalWrite(this->nTransmitterPin, LOW);
   }
 
   // Disable transmit after sending (i.e., for inverted protocols)
